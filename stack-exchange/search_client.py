8a667aaf7d2d99917b7342469f4fbb8ccb381a55
mike <mike@DESKTOP-1B4RV8T.localdomain>
2022-06-26 20:41:41 -0400

    Started fast search client

--- stack-exchange/search_client.py
+++ stack-exchange/search_client.py
@@ -2,6 +2,27 @@
 Stack exchange client interface used for searching!
 """
 
+import requests
+from dataclasses import dataclass
+from .errors import StackRequestError, ZeroSearchResultsError
+from typing import Optional, List
+
+
+@dataclass(frozen=True)
+class SearchResult:
+    question_title: str
+    question_body: str
+    answer: str
+    is_answered: bool
+    creation_date: str
+    score: int
+
+    @classmethod
+    def from_raw_response(cls, search_response: dict, answer: str) -> 'SearchResult':
+        r = search_response
+        return cls(question_title=r['title'], question_body=r['body'], answer=answer, is_answered=r['is_answered'],
+                   creation_date=r['creation_date'], score=r['score'])
+
 
 class StackExchange:
     """
@@ -11,34 +32,56 @@ class StackExchange:
 
     SEARCH_ENDPOINT = "/search/advanced"
 
-    def __init__(self, version="2.3"):
-        self.version = version
-        self.url = f"https://api.stackexchange.com/{version}"
+    def __init__(self, version: str = "2.3") -> None:
+        self.__version = version
+        self.__url = f"https://api.stackexchange.com/{version}"
 
-    def _make_request(self, endpoint, params):
-        url = self.url + endpoint
-        response = requests.get(url, params).json()
+    def _make_request(self, endpoint: str, params: dict) -> dict:
+        """ Make a GET request to the given stack exchange endpoint with the provided query params """
+        url = self.__url + endpoint
+        response = requests.get(url, params)
+        response_dict = response.json()
 
-        if response.get('error_message') is not None:
-            raise StackRequestError(response.get('error_message'))
-        return response
+        if response_dict.get('error_message') is not None:
+            raise StackRequestError(f"Request FAILED to url: {response.url} \n Error: {response_dict['error_message']}")
+        return response_dict
 
-    def search(self, query, params, site="stackoverflow"):
-        default_params = {"accepted": True, "filter": "withbody"}
-        params = {**params, **default_params}
+    # Refactor search to use kwargs or config object?
+    # Inbody = the query string is found in the text body
+    def search(self, query: str, params: dict = Optional[None], site: str = "stackoverflow", answers: int = 1,
+               accepted: bool = True, in_body: bool = False, tags: Optional[List[str]] = None,
+               sort: str = "votes") -> str:
+        params = {"accepted": accepted, "filter": "withbody", "site": site, "answers": answers, "sort": sort}
 
-        resp = self._make_request(endpoint=self.SEARCH_ENDPOINT, params=params)
+        if in_body:
+            # query string must be in body of text
+            params.update({"body": query})
 
-        res = resp['items'][0]
-        print(res['title'])
-        question = res['body']
-        console.print(strip_tags(question))
+        # semi colon delimited list of tags, at least one has to be present resutls
+        if tags is not None:
+            params.update({"tagged": tags})
+            del params['q']  # remove query since we're searching by body... which makes more sense i think!
 
-        answer_id = res['accepted_answer_id']
+        response = self._make_request(endpoint=self.SEARCH_ENDPOINT, params=params)
+
+        if not response['items']:
+            raise ZeroSearchResultsError("No search results found.")
+
+        answer_id = response['items'][0]['accepted_answer_id']
 
         answer = self._make_request(f"/answers/{answer_id}", params={"site": site,
                                                                      "filter": "withbody"})
+        search_result = SearchResult.from_raw_response(response['items'][0], answer['items'][0]['body'])
 
-        print("***ANSWER*** \n")
-        a = answer['items'][0]['body']
-        # console.print(strip_tags(a))
+        return search_result
+
+        # res = resp['items'][0]
+        # print(res['title'])
+    #  question = res['body']
+    # console.print(strip_tags(question))
+    #
+
+    #
+    # print("***ANSWER*** \n")
+    # a = answer['items'][0]['body']
+    # # console.print(strip_tags(a))
