fcbf4d988514f921e3ebf81c6142eba0331ec2a7
myke2424 <myke2424@gmail.com>
2022-06-28 10:56:36 -0400

    git test

--- stack-exchange/errors.py
+++ stack-exchange/errors.py
@@ -1,6 +1,6 @@
 class StackRequestError(IOError):
-    """ Raise exception when an error occurred when making a request to the StackExchange API """
+    """Raise exception when an error occurred when making a request to the StackExchange API"""
 
 
 class ZeroSearchResultsError(RuntimeError):
-    """ Raise exception when a stack exchange request responds with zero search results """
+    """Raise exception when a stack exchange request responds with zero search results"""
