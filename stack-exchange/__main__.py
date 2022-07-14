bf0746ac2cbbe2afa48e27df63ed9327f8b41d07
myke2424 <myke2424@gmail.com>
2022-07-14 15:38:50 -0400

    Cleaned up main

--- stack-exchange/__main__.py
+++ stack-exchange/__main__.py
@@ -1,89 +1,33 @@
+import argparse
 import logging
 
 from . import commands, utils
 from .cache import RedisCache
 from .search import CachedStackExchange, StackExchange
-from .tui import MyApp
+from .terminal import Terminal
+from .search import SearchRequest, get_stack_exchange_service
+from .search import Searchable
+from .models import Config, RedisConfig
 
-config_file_path = "config.yaml"
-config = utils.load_yaml_file(config_file_path)
-
-log_level = config["logging"]["log_level"]
-logging.basicConfig(level=log_level)
-logger = logging.getLogger(__name__)
-logger.setLevel(log_level)
-
-"""
-Builder Pattern PoC for Building a Search Request
-
-
-sr = SearchRequestBuilder(site="stackoverflow")
-                    .query("Merge two dicts python")
-                    .tags("python", "dictionary")
-                    .count(5)
-                    .accepted() 
-                            
-This doesn't feel idiomatic...
-
-Maybe Builder Pattern for SearchResult
-
-search_result = SearchResultBuilder()
-                        .with_question()
-                        .with_accepted_answer()
-                        .with_non_accepted_answers()
-                        .with_comments()
-
-
-"""
-
-from rich.markdown import Markdown
-from rich.console import Console
-from rich.text import Text
-from rich import print as p
+CONFIG_FILE_PATH = "config.yaml"
+N_RESULTS = 10
 
 
 def main():
-    stack_exchange = StackExchange()
-    console = Console()
-    # Create cached search client if redis config
-    # Load config file into immutable dataclass might be better after all...
-    if config["redis"]["host"] and config["redis"]["port"] and config["redis"]["password"]:
-        redis_cache = RedisCache(**config["redis"])
-        stack_exchange = CachedStackExchange(stack_exchange_service=StackExchange(), cache=redis_cache)
-
+    """Main function to run the application"""
     args = commands.get_cmd_args()
-    logger.info(f"Command line arguments: {args}")  # TODO: Change to debug, maybe add verbose log flag
-    # INTERACTIVE SEARCH START
+    config = Config.from_yaml_file(CONFIG_FILE_PATH)
+    stack_exchange = get_stack_exchange_service(config)
 
-    # Right now i have to do --i=true, change it so i just do -i and and the flag will be true...
-    if args.interactive:
-        search_results = stack_exchange.search(query=args.query, site=args.site, num=10)
-        p(f"\n[bold green]Search results for query: {''.join(args.query)} \n")
-        for idx, result in enumerate(search_results):
-            p(f"{idx + 1}.[bold red]{result.question.title}")
+    search_request = (
+        SearchRequest.Builder(args.query, args.site).with_tags(args.tags).accepted_only().n_results(N_RESULTS).build()
+    )
 
-        print("\n")
-        selected_q = int(input("Enter question number to see answer: "))
+    search_results = stack_exchange.search(search_request)
 
-        p(f"[bold red]Question: [bold green]{search_results[selected_q - 1].question.title} \n")
-        console.print(utils.html_to_markdown(search_results[selected_q - 1].question.body))
+    terminal = Terminal(interactive_search=args.interactive)
+    terminal.run(search_results)
 
-        p(f"\n[bold red]Top Answer: [bold red] \n")
-        console.print(utils.html_to_markdown(search_results[selected_q - 1].answer.body))
-
-    # INTERACTIVE SEARCH END
-    else:
-        # FAST SEARCH START
-        search_results = stack_exchange.search(query=args.query, site=args.site)
-
-        p(f"[bold red]Question: [bold green]{search_results[0].question.title} \n")
-        console.print(utils.html_to_markdown(search_results[0].question.body))
-
-        p(f"[bold red]Top Answer: [bold red] \n")
-        console.print(utils.html_to_markdown(search_results[0].answer.body))
-        # FAST SEARCH END
-
-        p(f"\n[bold green]Press [SPACE] to see more results, or any other key to exit [bold red] \n")
 
 if __name__ == "__main__":
     main()
